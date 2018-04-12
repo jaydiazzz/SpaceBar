895747a8bb5b5ae6dc57e3720aa49f01ba40a455
Jay <jaydiazzz10@gmail.com>
2018-04-12 14:26:19 -0700

    made a modal and some javascript changes along with nav fixes

--- public/js/index.js
+++ public/js/index.js
@@ -75,3 +75,11 @@ else if(n === "Saturday"){
       ss[i].style.color = green;
   }
 }
+
+document.querySelector(".gtdr").onclick = function(){
+  document.querySelector("#modalback").classList.add("show");
+}
+
+document.querySelector(".fa-times-circle").onclick = function(){
+  document.querySelector("#modalback").classList.remove("show");
+}
